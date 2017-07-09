19113d64ff53b3b5bbe956b22421e2ecb460d905
Alana Gilston <alana@abluescarab.us>
2017-07-09 04:43:52 -0700

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * todo: set Properties > Debug > Start Action to tModLoader Terraria.exe
+ * todo: set Properties > Debug > Working directory to Terraria folder
+ * todo: update description.txt
+ */
+
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace $safeprojectname$ {
+    class $safeprojectname$ : Mod {
+        public $safeprojectname$() {
+            Properties = new ModProperties() {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            }
+        }
+    }
+}
